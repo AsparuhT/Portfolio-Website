4bfce8d2cee77dca1d1b4a5a793b0e58aa2bcad5
Aspatuh Tenev <me@atenev.com>
2023-09-21 16:45:13 +0300

    before replacing the card divs with anchors

--- js/nav-hover-animation.js
+++ js/nav-hover-animation.js
@@ -22,22 +22,25 @@ projectsLinks.forEach(link => {
         // Todo
         if (e.target.getAttribute('data-card') === 'card-1') {
             e.target.style.color = "#ff014f";
-            todoCard.style.border = "3px solid #ff014f";
+            todoCard.style.border = "4px solid #ff014f";
         }
 
         // Gallery
         if (e.target.getAttribute('data-card') === 'card-2') {
-            galleryCard.style.border = "3px solid #ff014f";
+            e.target.style.color = "#ff014f";
+            galleryCard.style.border = "4px solid #ff014f";
         }
 
         // GitHub
         if (e.target.getAttribute('data-card') === 'card-3') {
-            gitCard.style.border = "3px solid #ff014f";
+            e.target.style.color = "#ff014f";
+            gitCard.style.border = "4px solid #ff014f";
         }
 
         // Weather
         if (e.target.getAttribute('data-card') === 'card-4') {
-            weatherCard.style.border = "3px solid #ff014f";
+            e.target.style.color = "#ff014f";
+            weatherCard.style.border = "4px solid #ff014f";
         }
     })
 })
@@ -52,25 +55,31 @@ projectsLinks.forEach(link => {
         // Todo
         if (e.target.getAttribute('data-card') === 'card-1') {
             e.target.style.color = "#212529";
-            todoCard.style.border = "none";
+            todoCard.style.border = "2px solid #808080";
         }
 
         // Galler
         if (e.target.getAttribute('data-card') === 'card-2') {
-            galleryCard.style.border = "none";
+            e.target.style.color = "#212529";
+            galleryCard.style.border = "2px solid #808080";
         }
 
         // GitHub
         if (e.target.getAttribute('data-card') === 'card-3') {
-            gitCard.style.border = "none";
+            e.target.style.color = "#212529";
+            gitCard.style.border = "2px solid #808080";
         }
 
         // Weather
         if (e.target.getAttribute('data-card') === 'card-4') {
-            weatherCard.style.border = "none";
+            e.target.style.color = "#212529";
+            weatherCard.style.border = "2px solid #808080";
         }
     })
 })
+
+
+
 
 
 
@@ -81,17 +90,51 @@ projectsLinks.forEach(link => {
 
 // When mouse on
 todoCard.addEventListener('mouseenter', () => {
-    todoCard.style.border = "3px solid #ff014f";
+    todoCard.style.border = "4px solid #ff014f";
     projectsLinks[0].style.color = "#ff014f";
 })
+
+galleryCard.addEventListener('mouseenter', () => {
+    galleryCard.style.border = "4px solid #ff014f";
+    projectsLinks[1].style.color = "#ff014f";
+})
+
+gitCard.addEventListener('mouseenter', () => {
+    gitCard.style.border = "4px solid #ff014f";
+    projectsLinks[2].style.color = "#ff014f";
+})
+
+weatherCard.addEventListener('mouseenter', () => {
+    weatherCard.style.border = "4px solid #ff014f";
+    projectsLinks[3].style.color = "#ff014f";
+})
+
 
 
 
 // When mouse off
 todoCard.addEventListener('mouseleave', () => {
-    todoCard.style.border = "none";
+    todoCard.style.border = "2px solid #808080";
     projectsLinks[0].style.color = "#212529";
 })
+
+galleryCard.addEventListener('mouseleave', () => {
+    galleryCard.style.border = "2px solid #808080";
+    projectsLinks[1].style.color = "#212529";
+})
+
+
+gitCard.addEventListener('mouseleave', () => {
+    gitCard.style.border = "2px solid #808080";
+    projectsLinks[2].style.color = "#212529";
+})
+
+
+weatherCard.addEventListener('mouseleave', () => {
+    weatherCard.style.border = "2px solid #808080";
+    projectsLinks[3].style.color = "#212529";
+})
+
 
 
 
