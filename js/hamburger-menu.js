589b6ce11bc40e0523e8525184b0e965c54e78bc
Aspatuh Tenev <me@atenev.com>
2023-09-21 21:51:50 +0300

    the menu seems to be ready too

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -5,11 +5,16 @@
 
 
 
-document.getElementById('menu-toggle').addEventListener('click', function() {
-  const menu = document.getElementById('menu');
-  const menuIcon = document.getElementById('menu-toggle');
+// document.getElementById('menu-toggle').addEventListener('click', function() {
+//   const menu = document.getElementById('menu');
+//   const menuIcon = document.getElementById('menu-toggle');
   
-  menu.classList.toggle('hidden');
-  menuIcon.classList.toggle('toggle'); // Toggles the class to change the icon to 'X' and back
-});
+//   menu.classList.toggle('hidden');
+//   menuIcon.classList.toggle('toggle'); // Toggles the class to change the icon to 'X' and back
+// });
 
+
+document.querySelector('.menu-icon').addEventListener('click', function () {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('visible');
+});
